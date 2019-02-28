3641d0d688b806f95fae1098e9091619fbb4233c
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-28 11:24:18 +0000

    Booking class created and tested

---
+++ src/test/java/com/example/coursebooker/BookingTest.java
@@ -0,0 +1,31 @@
+package com.example.coursebooker;
+
+import models.Booking;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookingTest {
+
+    Booking booking;
+
+    @Before
+    public void before(){
+        booking = new Booking("24-12-2020");
+    }
+
+
+    @Test
+    public void canGetDate(){
+        assertEquals("24-12-2020", booking.getDate());
+    }
+
+    @Test
+    public void canSetDate(){
+        booking.setDate("25-12-2020");
+        assertEquals("25-12-2020", booking.getDate());
+    }
+
+}
+
